2849621d0c8812e2af4896b1eb63baba591a90c0
Eros-afk <erosryansimette@alu.ufc.br>
2026-01-20 21:45:12 -0300

    primeiras aulas de java

---
+++ Aula37/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        int farinha = 2;
+        System.out.println("Areceita requer " + farinha + " xicaras de farinha.");
+    }
+}
---
+++ Aula38/main.java
@@ -0,0 +1,21 @@
+public class main{
+    public static void main(String[] args){
+        byte idade = 25;
+        short ano = 2024;
+        int populacao = 20766000;
+        long distancia = 15000000000L;
+        float altura = 1.75f;
+        double peso = 70.5;
+        boolean estudante = true;
+        char genero = 'M';
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("População da cidade: " + populacao);
+        System.out.println("População mundial: " + populacao);
+        System.out.println("Altura: " + altura);
+        System.out.println("Peso: " + peso);
+        System.out.println("É estudante? " + estudante);
+        System.out.println("Gênero: " + genero);
+    }
+}
