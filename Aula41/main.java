eb5bd3f3fcec6bd719375d0c1e013da0b42c5022
Eros-afk <erosryansimette@alu.ufc.br>
2026-01-21 07:59:01 -0300

    #

---
+++ Aula41/main.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String[] args){
+        var numero = 10;
+        int numero2 = numero; 
+
+        System.out.println("Valor do numero = " + numero);
+    }
+}
