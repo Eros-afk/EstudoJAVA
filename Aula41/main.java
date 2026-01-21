e5b4042fd9f2b701847548924e93812496d7b505
Eros-afk <erosryansimette@alu.ufc.br>
2026-01-21 08:35:12 -0300

    exemplo1

--- Aula41/main.java
+++ Aula41/main.java
@@ -3,6 +3,23 @@ public class main{
         var numero = 10;
         int numero2 = numero; 
 
-        System.out.println("Valor do numero = " + numero);
+        System.out.println("Valor do numero = " + numero2);
+
+        //Comentário 
+
+        /*
+        Comentario 
+        a
+        b
+        c
+        */
+
+        /** 
+        * Comentário Java Doc
+       */
+
+        var numero3 = 10.5;
+        double numero4 = numero3;
+        System.out.println("Valor do numero = " + numero4);
     }
 }
---
+++ Exercicio1/ControleEstoquePadaria.java
@@ -0,0 +1,9 @@
+public class ControleEstoquePadaria{
+    public static void main(String[] args) {
+        int qauntidadeFarinha = 10;
+        int qauntidadeAcucar = 5;
+
+        System.out.println("Farinha: " + qauntidadeFarinha);
+        System.out.println("Acucar: " + qauntidadeAcucar);
+    }
+}
