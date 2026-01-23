a6f7e0dcaee1ef016465f526c5c50b3d37195d5a
Eros-afk <erosryansimette@alu.ufc.br>
2026-01-23 19:35:21 -0300

    private

--- Aula102/FolhaDePagamento.java
+++ Aula102/FolhaDePagamento.java
@@ -3,7 +3,6 @@ import empresa.dados.Funcionario;
 
 public class FolhaDePagamento{
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario("Ana", 3500.00);
-        System.out.println("Nome: " + funcionario.nome);
+        Funcionario funcionario = new Funcionario();
     }
 }
--- Aula102/empresa/dados/Funcionario.java
+++ Aula102/empresa/dados/Funcionario.java
@@ -1,13 +1,17 @@
 package empresa.dados;
 
 public class Funcionario{
-    public String nome;
+    private String nome;
     double salario;
 
-    public Funcionario(String nome, double salario){
+    private Funcionario(String nome, double salario){
         this.nome = nome;
         this.salario = salario;
         System.out.println("Nome: " + nome);
         System.out.println("Salário: " + salario);
+    }
+    
+    public Funcionario(){
+        Funcionario func = new Funcionario("Roberto", 4500.00);
     }
 }
